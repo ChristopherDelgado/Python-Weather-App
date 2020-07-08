82cad92fea7dfac69c84c71f9166d9807aea0582
Christopher <Christopher.delgado.754@my.csun.edu>
2020-07-08 01:10:13 -0700

    This is the main file for the application, this is what you run.

---
+++ main.py
@@ -0,0 +1,8 @@
+# importing GUI as the main graphics portion of the app
+# import tkinter as the input for the root of Frame.__init__()
+
+import GUI as graphics
+import tkinter as tk
+
+frame = graphics.Frame(tk.Tk())
+frame.input()
